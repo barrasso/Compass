b9b6d2e13b7b9d4f9f57e6807b918d80fdcec3cd
Mark Barrasso <mark.barrasso@live.com>
2015-02-22 13:36:12 -0500

    initial project setup

---
+++ Compass/LoginViewController.swift
@@ -0,0 +1,45 @@
+//
+//  LoginViewController.swift
+//  Compass
+//
+//  Created by Mark on 2/22/15.
+//  Copyright (c) 2015 MEB. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+
+    @IBOutlet var signupTriangle: UIImageView!
+    @IBOutlet var loginTriangle: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func signupButtonToggled(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func loginButtonToggled(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func arrowButtonSelected(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func facebookButtonSelected(sender: AnyObject) {
+    }
+    
+}
